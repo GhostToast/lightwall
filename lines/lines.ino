aa29088b15977786009c674fd4a73528484f2db8
Gustave F. Gerhardt <gus@thewirecutter.com>
2018-04-22 15:31:24 -0500

    Experiment with random

--- lines/lines.ino
+++ lines/lines.ino
@@ -2,10 +2,17 @@
 #include <Adafruit_NeoMatrix.h>
 #include <Adafruit_NeoPixel.h>
 
-#define PIN 6
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
 
-#define WIDTH 56
-#define HEIGHT 32
+const int PIN        =   6;
+const int WIDTH      =  56; // Actual row in pixels.
+const int HEIGHT     =  32; // Fake: accounts for 2*8 additional empty pixels per column.
+const int BRIGHTNESS =  50;
+const int NUM_LEDS   = 896;
+
+long lastAnimation = 0;
 
 /**
  * Setup the lightwall matrix.
@@ -17,30 +24,6 @@ Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(WIDTH, HEIGHT,PIN,
   NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG,
   NEO_GRBW           + NEO_KHZ800
 );
-
-/**
- * LED order in a given block.
- */
-const uint16_t block[][8] = {
-  {0,  15,  16,  31,  32,  47,  48,  63},
-  {1,  14,  17,  30,  33,  46,  49,  62},
-  {2,  13,  18,  29,  34,  45,  50,  61},
-  {3,  12,  19,  28,  35,  44,  51,  60},
-  {4,  11,  20,  27,  36,  43,  52,  59},
-  {5,  10,  21,  26,  37,  42,  53,  58},
-  {6,   9,  22,  25,  38,  41,  54,  57},
-  {7,   8,  23,  24,  39,  40,  55,  56}
-};
-
-/**
- * The entire grid, including fake panels (denoated with -1).
- */
-const uint16_t grid[][7] = {
-  { 0,  -1,   4,  -1,   8,  -1,  12},
-  {-1,   2,  -1,   6,  -1,  10,  -1},
-  { 1,  -1,   5,  -1,   9,  -1,  13},
-  {-1,   3,  -1,   7,  -1,  11,  -1}
-};
 
 uint16_t remapXY(uint16_t x, uint16_t y) {
 
@@ -82,15 +65,16 @@ void setup() {
  */
 void loop() {
   matrix.fillScreen(0);
-  matrix.clear();
-  for (uint8_t i = 0; i<=WIDTH; i++) {
-    if ( i%2 ) {
-      matrix.drawFastVLine(i, 0, HEIGHT, matrix.Color(0,min(4.5*i,255),0)); 
-    } else {
-      matrix.drawFastVLine(WIDTH-i, 0, HEIGHT, matrix.Color(0,0,min(4.5*i,255))); 
-    }
-  }
-  matrix.show();
+  theMatrix(100);
 }
 
+void theMatrix(uint8_t matrixSpeed){
+  uint8_t x = random(0,matrix.width());
+  for (uint8_t y=0;y<=matrix.height();y++){
+    if(millis() - lastAnimation > matrixSpeed) {
+      matrix.drawPixel(x,y, matrix.Color(0,200,0));
+      matrix.show();
+    }
+  }
+}
 

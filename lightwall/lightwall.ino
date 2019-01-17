cf22066bba81f3cd9002fa7f54cf39e263b6966c
Gustave F. Gerhardt <gfgerhardt@gmail.com>
2019-01-16 19:30:24 -0600

    Changes for python serial

--- lightwall/lightwall.ino
+++ lightwall/lightwall.ino
@@ -1,15 +1,6 @@
 #include <OctoSK6812.h>
-#include <EEPROM.h>
 #include "rainColumn.h"
 #include "utilities.h"
-
-const int ledsPerStrip = 128;
-#define NUM_LEDS 896
-#define BRIGHTNESS 50
-DMAMEM int displayMemory[ledsPerStrip*8];
-int drawingMemory[ledsPerStrip*8];
-
-OctoSK6812 leds(ledsPerStrip, displayMemory, drawingMemory, SK6812_GRBW);
 
 /**
    LED order in a given block.
@@ -35,48 +26,165 @@ const int grid[4][7] = {
   {-1,   3,  -1,   7,  -1,  11,  -1}
 };
 
-#define maxChars 32
 rainColumn allRainColumns[56]; // Array to hold all rainColumn structs.
 uint8_t maxWidth = 56;
 uint8_t maxHeight = 32;
-char inputString[maxChars];
-char currentCharacter;
-int inputIndex = 0;
-int displayFlag = 0;
-char currentColorChannel;
-char displayPattern;
 char matrixColorMode = 'g';
-byte rVal;
-byte bVal;
-byte gVal;
-byte wVal;
-unsigned long eepromThrottleInterval = 5000;
-unsigned long eepromLastUpdate = 0;
+uint8_t matrixColors[4][2];
 unsigned long currentTime = 0;
+unsigned long fadeLastTime = 0;
+byte fadeSteps = 32;
+byte fadeIndex = 0;
+byte fadeInterval = 10;
+const byte buffSize = 40;
+char inputBuffer[buffSize];
+const char startMarker = '<';
+const char endMarker = '>';
+byte bytesRecvd = 0;
+boolean readInProgress = false;
+boolean newDataFromServer = false;
+char messageFromServer[buffSize] = {0};
+byte userMode = 0;
+byte rVal = 0;
+byte gVal = 0;
+byte bVal = 0;
+byte wVal = 0;
+byte rValOld = 0;
+byte gValOld = 0;
+byte bValOld = 0;
+byte wValOld = 0;
 
-// Setup it up.
+const int ledsPerStrip = 128;
+#define NUM_LEDS 896
+#define BRIGHTNESS 50
+DMAMEM int displayMemory[ledsPerStrip * 8];
+int drawingMemory[ledsPerStrip * 8];
+
+OctoSK6812 leds(ledsPerStrip, displayMemory, drawingMemory, SK6812_GRBW);
+
 void setup() {
   Serial.begin(9600);
   leds.begin();
   leds.show();
-
-  currentTime = millis();
-  //readEEPROM();
 }
 
-// The main loop.
 void loop() {
-  //processUserInput();
-  //displayUserSelectedMode();
-  //rainbowCycle(768, 1, 1 );
-  //holidayLights();
-  makeItRain();
-  //Serial.println("hello server");
+  currentTime = millis();
+  processUserInput();
+  respondToServer();
+  displayUserSelectedMode();
+}
+
+void processUserInput() {
+  if ( Serial.available() > 0) {
+    char x = Serial.read();
+
+    if (x == endMarker) {
+      readInProgress = false;
+      newDataFromServer = true;
+      inputBuffer[bytesRecvd] = 0;
+      parseData();
+    }
+
+    if (readInProgress) {
+      inputBuffer[bytesRecvd] = x;
+      bytesRecvd ++;
+      if (bytesRecvd == buffSize) {
+        bytesRecvd = buffSize - 1;
+      }
+    }
+
+    if (x == startMarker) {
+      bytesRecvd = 0;
+      readInProgress = true;
+    }
+  }
+}
+
+void parseData() {
+  // Used as index by strtok().
+  char * strtokIndex;
+
+  // Get first part, should inform what mode this will be.
+  strtokIndex = strtok(inputBuffer, ",");
+  if ( strcmp(strtokIndex, "state") == 0) {
+    // Do not change user mode, as this request is attempting to receive current state.
+    processState();
+  } else if (strcmp(strtokIndex, "rgbw") == 0) {
+    userMode = 1;
+    processRGBW(strtokIndex);
+  } else if (strcmp(strtokIndex, "matrix") == 0) {
+    userMode = 2;
+    processMatrix(strtokIndex);
+  }
+}
+
+void processState() {
+  if (1 == userMode){
+    Serial.print("<rgbw,");
+    Serial.print(rVal);
+    Serial.print(",");
+    Serial.print(gVal);
+    Serial.print(",");
+    Serial.print(bVal);
+    Serial.print(",");
+    Serial.print(wVal);
+    Serial.println(">");
+  } else if (2 ==userMode) {
+    Serial.print("<matrix,");
+    Serial.print(matrixColorMode);
+    Serial.print(">");
+  } else {
+    //Serial.print("<fail>");
+    Serial.println(1);
+  }
+}
+
+void processRGBW(char * strtokIndex) {
+  rValOld = rVal;
+  gValOld = gVal;
+  bValOld = bVal;
+  wValOld = wVal;
+  fadeIndex = 0;
+
+  // Get the next part, which should be Red value.
+  strtokIndex = strtok(NULL, ",");
+  rVal = atoi(strtokIndex);
+
+  // Get next part, which should be Green value.
+  strtokIndex = strtok(NULL, ",");
+  gVal = atoi(strtokIndex);
+
+  // Get next part, which should be Blue value.
+  strtokIndex = strtok(NULL, ",");
+  bVal = atoi(strtokIndex);
+
+  // Get next part, which should be White value.
+  strtokIndex = strtok(NULL, ",");
+  wVal = atoi(strtokIndex);
+}
+
+void processMatrix(char * strtokIndex) {
+  // Fill up matrix colors.
+  for (byte i = 0; i < 4; i++) {
+    for (byte z = 0; z <2; z++) {
+      strtokIndex = strtok(NULL, ",");
+      matrixColors[i][z] = atoi(strtokIndex);
+    }
+  }
+}
+
+void respondToServer() {
+  if (newDataFromServer) {
+    newDataFromServer = false;
+    Serial.println(userMode);
+  }
 }
 
 void makeItRain() {
   // Keep things randomized.
-  reseedRandomness();
+  // TODO get random
+  //reseedRandomness(); 
 
   // Initialize matrix if this is first run.
   static boolean matrixInitialized = false;
@@ -97,16 +205,6 @@ void makeItRain() {
   leds.show();
 }
 
-// Occasionally reseed our randomness. (30 seconds).
-void reseedRandomness() {
-  static long randomnessLastSeed = 0;
-  currentTime = millis();
-  if ( currentTime - randomnessLastSeed >= 30000 ) {
-    randomSeed(analogRead(0));
-    randomnessLastSeed = currentTime;
-  }
-}
-
 // Assign rain Column Properties. Mostly random, maintain column, lastUpdated, and lastCompleted.
 void assignColumnProperties( rainColumn &rainColumn ) {
   rainColumn.head = 0;
@@ -116,29 +214,37 @@ void assignColumnProperties( rainColumn &rainColumn ) {
   rainColumn.canGoBlack = random(0, 2);
   rainColumn.dimAmount = random(2, 8);
 
-  switch (matrixColorMode) {
-    case 'r':
-      rainColumn.color = makeColor(random(192, 256), random(0, 8), random(0, 8), 0, BRIGHTNESS);
-      break;
-    case 'g':
-      rainColumn.color = makeColor(random(0, 24), random(192, 256), random(0, 32), 0, BRIGHTNESS);
-      break;
-    case 'b':
-      rainColumn.color = makeColor(random(0, 8), random(24, 96), random(192, 256), 0, BRIGHTNESS);
-      break;
-    case 'w':
-      rainColumn.color = makeColor(0, 0, 0, random(96, 256));
-      break;
-    case 'o': // orange.
-      rainColumn.color = makeColor(random(96, 128), random(96, 128), random(0, 32), 0, BRIGHTNESS);
-      break;
-    case 'p': // purple.
-      rainColumn.color = makeColor(random(192, 256), random(0, 24), random(192, 256), 0, BRIGHTNESS);
-      break;
-    default:
-      rainColumn.color = makeColor(random(0, 8), random(24, 96), random(192, 256), 0, BRIGHTNESS);
-      matrixColorMode = 'b';
-  }
+  rainColumn.color = makeColor(
+    random(matrixColors[0][0], matrixColors[0][1]),
+    random(matrixColors[1][0], matrixColors[1][1]),
+    random(matrixColors[2][0], matrixColors[2][1]),
+    random(matrixColors[3][0], matrixColors[3][1]),
+    BRIGHTNESS
+  );
+
+//  switch (matrixColorMode) {
+//    case 'r':
+//      rainColumn.color = makeColor(random(192, 256), random(0, 8), random(0, 8), 0, BRIGHTNESS);
+//      break;
+//    case 'g':
+//      rainColumn.color = makeColor(random(0, 24), random(192, 256), random(0, 32), 0, BRIGHTNESS);
+//      break;
+//    case 'b':
+//      rainColumn.color = makeColor(random(0, 8), random(24, 96), random(192, 256), 0, BRIGHTNESS);
+//      break;
+//    case 'w':
+//      rainColumn.color = makeColor(0, 0, 0, random(96, 256));
+//      break;
+//    case 'o': // orange.
+//      rainColumn.color = makeColor(random(96, 128), random(96, 128), random(0, 32), 0, BRIGHTNESS);
+//      break;
+//    case 'p': // purple.
+//      rainColumn.color = makeColor(random(192, 256), random(0, 24), random(192, 256), 0, BRIGHTNESS);
+//      break;
+//    default:
+//      rainColumn.color = makeColor(random(0, 8), random(24, 96), random(192, 256), 0, BRIGHTNESS);
+//      matrixColorMode = 'b';
+//  }
 
   rainColumn.interval = random(25, 115);
   rainColumn.sleepTime = random(1000, 3000);
@@ -159,7 +265,7 @@ void rainOneColumn( rainColumn &rainColumn ) {
 
       // Inform that the animation has terminated, and set lastCompleted time.
       rainColumn.isRunning = 0;
-      rainColumn.lastCompleted = millis();
+      rainColumn.lastCompleted = currentTime;
 
       // Build new properties so the next streamer in this column will not be identical to this one.
       assignColumnProperties( rainColumn );
@@ -204,182 +310,6 @@ void updateRainColumnFrame(rainColumn &rainColumn) {
   rainColumn.lastUpdated = currentTime;
 }
 
-// Listen for user input and process it, populating variables.
-void processUserInput() {
-  while (Serial.available()) {
-    displayFlag = 0;
-    if (inputIndex < maxChars - 1) {
-      currentCharacter = Serial.read();
-      inputString[inputIndex] = currentCharacter;
-      processCharacter();
-      inputIndex++;
-      inputString[inputIndex] = '\0'; // Always null terminate.
-    }
-  }
-}
-
-// Render user's choice.
-void displayUserSelectedMode() {
-  if (displayFlag == 0) {
-    displayFlag = 1;
-    inputIndex = 0;
-    Serial.print(inputString); // Send debug back to phone.
-
-    // We only need to fire the "oneColor" once per instruction set, not continuously.
-    if (displayPattern == 's') {
-      oneColor(makeColor(rVal, gVal, bVal, wVal));
-    }
-  }
-  if (displayFlag == 1 && displayPattern == 'm') {
-    makeItRain();
-  }
-  //writeEEPROM();
-}
-
-// Processes current character, setting mode and colors accordingly.
-void processCharacter() {
-  if (currentCharacter == 's' || currentCharacter == 'm') {
-    displayPattern = currentCharacter;
-  } else if (displayPattern == 's') {
-    processSingleColorCharacter();
-  } else if (displayPattern == 'm') {
-    matrixColorMode = currentCharacter;
-  }
-}
-
-// Processes current character, setting up color for single color display.
-void processSingleColorCharacter() {
-  if (currentCharacter == 'r') {
-    currentColorChannel = currentCharacter;
-    rVal = 0;
-  } else if (currentCharacter == 'g') {
-    currentColorChannel = currentCharacter;
-    gVal = 0;
-  } else if (currentCharacter == 'b') {
-    currentColorChannel = currentCharacter;
-    bVal = 0;
-  } else if (currentCharacter == 'w') {
-    currentColorChannel = currentCharacter;
-    wVal = 0;
-  } else if (currentColorChannel == 'r') {
-    rVal *= 10;
-    rVal += currentCharacter - '0';
-  } else if (currentColorChannel == 'g') {
-    gVal *= 10;
-    gVal += currentCharacter - '0';
-  } else if (currentColorChannel == 'b') {
-    bVal *= 10;
-    bVal += currentCharacter - '0';
-  } else if (currentColorChannel == 'w') {
-    wVal *= 10;
-    wVal += currentCharacter - '0';
-  }
-}
-
-// Occasionally write our color information to the EEPROM so it can survive a power cycle.
-void writeEEPROM() {
-  currentTime = millis();
-  if ( (currentTime - eepromLastUpdate ) >= eepromThrottleInterval ) {
-    EEPROM.update(0, displayPattern);
-    if (displayPattern == 's') {
-      EEPROM.update(1, rVal);
-      EEPROM.update(2, gVal);
-      EEPROM.update(3, bVal);
-      EEPROM.update(4, wVal);
-    } else if (displayPattern == 'm') {
-      EEPROM.update(1, matrixColorMode);
-    }
-
-    eepromLastUpdate = currentTime;
-  }
-}
-
-// Attempt to read EEPOM state when booting up, to use last known pattern and color.
-void readEEPROM() {
-  displayPattern = EEPROM.read(0);
-
-  if (displayPattern == 's') {
-    rVal = EEPROM.read(1);
-    gVal = EEPROM.read(2);
-    bVal = EEPROM.read(3);
-    wVal = EEPROM.read(4);
-  } else if (displayPattern == 'm') {
-    matrixColorMode = EEPROM.read(1);
-  }
-}
-
-/**
-   Color Utilities.
-*/
-// Instructs all LED to display the same color, then renders.
-void oneColor(uint32_t color) {
-  for (uint16_t i = 0; i < NUM_LEDS; i++) {
-    leds.setPixel(i, color);
-  }
-  leds.show();
-}
-
-// Calculate diminishing version of a color.
-uint32_t dimColor(uint32_t color, byte dimAmount, bool canGoBlack) {
-  // Subtract R, G and B components until zero, except dominant color.
-  uint8_t r = max( 0, red(color) - dimAmount );
-  uint8_t g = max( 0, green(color) - dimAmount );
-  uint8_t b = max( 0, blue(color) - dimAmount );
-  uint8_t w = 0;
-
-  if ( ! canGoBlack ) {
-    // Red.
-    if (matrixColorMode == 'r' && b <= dimAmount ) {
-      b = b + dimAmount;
-    }
-
-    // Green.
-    if (matrixColorMode == 'g' && g <= dimAmount ) {
-      g = g + dimAmount;
-    }
-
-    // Blue.
-    if (matrixColorMode == 'b' && b <= dimAmount ) {
-      b = b + dimAmount;
-    }
-
-    // White.
-    if (matrixColorMode == 'w' && b <= dimAmount ) {
-      w = w + dimAmount;
-    }
-
-    // Orange.
-    if (matrixColorMode == 'o') {
-      if (r <= dimAmount) {
-        r = r + dimAmount;
-      }
-      if (g <= dimAmount) {
-        g = g + dimAmount;
-      }
-    }
-
-    // Purple.
-    if (matrixColorMode == 'p') {
-      if (r <= dimAmount) {
-        r = r + dimAmount;
-      }
-      if (b <= dimAmount) {
-        b = b + dimAmount;
-      }
-    }
-  }
-
-  uint32_t dimColor = makeColor(r, g, b, w, 0);
-
-  return dimColor;
-}
-
-// Brighten a color by adding white.
-uint32_t lightenColor(uint32_t color, byte whiteAmount) {
-  uint32_t lightenColor = makeColor( red(color), green(color), blue(color), whiteAmount);
-  return lightenColor;
-}
-
 // Remap coordinates to an actual pixel number. Or a non-existent one.
 uint16_t remapXY(uint8_t x, uint8_t y) {
   if ( x >= maxWidth || y >= maxHeight || x < 0 || y < 0 ) {
@@ -398,46 +328,82 @@ uint16_t innerRemapXY(uint8_t x, uint8_t y, uint16_t pixelBlock) {
 }
 
 
-void rainbowCycle(uint32_t frames , uint32_t frameAdvance, uint32_t pixelAdvance ) {
 
-  // Hue is a number between 0 and 3*256 than defines a mix of r->g->b where
-  // hue of 0 = Full red
-  // hue of 128 = 1/2 red and 1/2 green
-  // hue of 256 = Full Green
-  // hue of 384 = 1/2 green and 1/2 blue
-  // ...
-  unsigned int firstPixelHue = 0;     // Color for the first pixel in the string
+// Calculate diminishing version of a color.
+uint32_t dimColor(uint32_t color, byte dimAmount, bool canGoBlack) {
+  uint8_t r1 = red(color);
+  uint8_t g1 = green(color);
+  uint8_t b1 = blue(color);
+  uint8_t w1 = white(color);
+  
+  // Subtract R, G and B components until zero, except dominant color.
+  uint8_t r2 = max( 0, r1 - dimAmount );
+  uint8_t g2 = max( 0, g1 - dimAmount );
+  uint8_t b2 = max( 0, b1 - dimAmount );
+  uint8_t w2 = max( 0, w1 - dimAmount );
 
-  for (unsigned int j = 0; j < frames; j++) {
-    unsigned int currentPixelHue = firstPixelHue;
+  uint32_t dimColor = makeColor(r2, g2, b2, w2);
 
-    for (unsigned int i = 0; i < NUM_LEDS; i++) {
-
-      if (currentPixelHue >= (3 * 256)) {              // Normalize back down incase we incremented and overflowed
-        currentPixelHue -= (3 * 256);
-      }
-
-      unsigned char phase = currentPixelHue >> 8;
-      unsigned char step = currentPixelHue & 0xff;
-
-      switch (phase) {
-        case 0:
-          leds.setPixel( i, makeColor( ~step, step,  0 ) );
-          break;
-
-        case 1:
-          leds.setPixel( i, makeColor( 0, ~step, step ) );
-          break;
-
-        case 2:
-          leds.setPixel( i, makeColor( step, 0, ~step ) );
-          break;
-      }
-
-      currentPixelHue += pixelAdvance;
-
+  if ( ! canGoBlack && dimColor <= 0) {
+    if (r2 == 0) {
+      r2 = r1;
     }
-    leds.show();
-    firstPixelHue += frameAdvance;
+    if (g2 == 0) {
+      g2 = g1;
+    }
+    if (b2 == 0) {
+      b2 = b1;
+    }
+    if (w2 == 0) {
+      w2 = w1;
+    }
+    dimColor = makeColor(r2, g2, b2, w2);
+  }
+
+  return dimColor;
+}
+
+// Brighten a color by adding white.
+uint32_t lightenColor(uint32_t color, byte whiteAmount) {
+  uint32_t lightenColor = makeColor( red(color), green(color), blue(color), whiteAmount);
+  return lightenColor;
+}
+
+// Instructs all LED to display the same color, then renders.
+void oneColor(uint32_t color, uint32_t fadeColor = -1) {
+  if (fadeColor >= 0 && color != fadeColor && fadeIndex <= fadeSteps) {
+      uint8_t r = ((red(fadeColor) * (fadeSteps - fadeIndex)) + (red(color) * fadeIndex)) / fadeSteps;
+      uint8_t g = ((green(fadeColor) * (fadeSteps - fadeIndex)) + (green(color) * fadeIndex)) / fadeSteps;
+      uint8_t b = ((blue(fadeColor) * (fadeSteps - fadeIndex)) + (blue(color) * fadeIndex)) / fadeSteps;
+      uint8_t w = ((white(fadeColor) * (fadeSteps - fadeIndex)) + (white(color) * fadeIndex)) / fadeSteps;
+      color = makeColor( r, g, b, w );
+      fadeIndex++;
+  }
+  for (uint16_t i = 0; i < NUM_LEDS; i++) {
+    leds.setPixel(i, color);
+  }
+  leds.show();
+}
+
+void displayUserSelectedMode() {
+  switch (userMode) {
+    case 0: // None, dim white.
+      oneColor(0x00000010);
+      break;
+
+    case 1: // RGBW.
+      if (currentTime - fadeLastTime >= fadeInterval) {
+        fadeLastTime = currentTime;
+        oneColor( makeColor( rVal, gVal, bVal, wVal ), makeColor( rValOld, gValOld, bValOld, wValOld ));
+      }
+      break;
+
+    case 2: // Matrix.
+      makeItRain();
+      break;
+
+    default:
+      oneColor(0x00000010);
+      break;
   }
 }
